f16e8ed6e854af1c6251b157905f600aea8cfc6f
psychowico <psychowico@ubuntu>
2012-10-21 11:44:42 +0200

    remove minify sample configuration

--- config/module.config.php
+++ config/module.config.php
@@ -8,39 +8,11 @@ return array(
         'development_mode'       => true,
         //persistent file, some data will be stored there in
         //xml format, need have write access
-        'persistent_file'   => __DIR__ . '/../../../data/minifier/config.xml',
-        'public_dir'        => __DIR__ . '/../../../public',
+        'persistent_file'   => './data/minifier/config.xml',
+        'public_dir'        => './public',
         'bundles'   => array(
-            'js'    => array(
-                //relative to /public dir
-                'output_dir'     => '/js/bundles',
-                'list'              => array(
-                    //keys will be used to generate file name,
-                    //in this case:
-                    //public/js/bundles/js_bundle-{md5-hash}.js
-                    'js_bundle' => array(
-                        //how output file name should be generated,
-                        //first arg is 'name', second is file md5,
-                        //default is '%s-%s', so this is only sample.
-                        //extension will be added automatically
-                        'filename'  => 'bundle_%s-%s',
-                        //if only one element at list - can be give as string,
-                        'sources' => array(
-                            '/js/test.js',
-                            '/js/compiled_coffee/test321.js',
-                            '/js/compiled_coffee/my_dir/in_test.js',
-                        ),
-                    ),
-                ),
-            ),
-            'css'    => array(
-                'output_dir' => '/css/bundles',
-                'list'       => array(
-                    'css_bundle' => array(
-                        'sources' => '/css/style.css',
-                    ),
-                ),
-            ),
+            'js'    => array(),
+            'css'    => array(),
         ),
     ),
 
